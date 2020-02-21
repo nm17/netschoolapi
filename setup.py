5603af320866b5c9307fb8cc1597ea279f014ff2
nm17 <dannevergame@gmail.com>
2020-02-21 21:34:04 +0400

    feat: bump version 1.4.0 → 1.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="netschoolapi",
-    version="1.4.0",
+    version="1.4.1",
     packages=["netschoolapi"],
     url="https://github.com/nm17/netschoolapi/",
     license="MIT",
