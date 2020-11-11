df7a811e2332ec32832a448198c43beb27b6ac45
Ivan Gorlanov <56786107+igorlanov@users.noreply.github.com>
2020-11-11 16:42:04 +0300

    chore: Change single-quotes to double-quotes

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email="dannevergame@gmail.com",
     description="A fully asynchronous API wrapper for NetSchool written in Python",
     long_description=open("README.md").read(),
-    install_requires=["httpx", "python-dateutil", 'dacite'],
+    install_requires=["httpx", "python-dateutil", "dacite"],
     python_requires=">=3.6",
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
 )
