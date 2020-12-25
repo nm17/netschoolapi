bbbf879c0d7843e1dcbf048dc29421b5dc1149f1
Иван Горланов <56786107+igorlanov@users.noreply.github.com>
2020-12-25 16:28:45 +0300

    refactor: удаление/украшение мусора

--- netschoolapi/utils.py
+++ netschoolapi/utils.py
@@ -1,13 +1,14 @@
 from json.decoder import JSONDecodeError
-from httpx import Response
-from .exceptions import NetSchoolAPIError
-from . import __version__
 
-_USER_AGENT = f"NetSchoolAPI/{__version__}"
+from httpx import Response
+
+from .exceptions import NetSchoolAPIError
 
 
 def _json_or_panic(response: Response) -> dict:
     try:
         return response.json()
     except JSONDecodeError:
-        raise NetSchoolAPIError(f"{response.status_code}: {response.url}: {response.content}") from None
+        raise NetSchoolAPIError(
+            f"{response.status_code}: {response.url.path}: {response.content}",
+        ) from None
