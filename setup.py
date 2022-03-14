337379d4135ff7b40ffaeb132e0a67abeeb24d33
megahomyak <58137927+megahomyak@users.noreply.github.com>
2022-03-14 20:04:15 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as readme:
 
 setup(
     name='netschoolapi',
-    version='7.1.3',
+    version='7.1.4',
     description='Асинхронный API-клиент для «Сетевого города»',
     long_description=long_description,
     long_description_content_type='text/markdown',
