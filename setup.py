b544bba076e860c8da1bb3c961240be0c56a3514
megahomyak <g.megahomyak@gmail.com>
2022-11-06 21:13:10 +0500

    Version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as readme:
 
 setup(
     name='netschoolapi',
-    version='7.1.8',
+    version='7.1.9',
     description='Асинхронный API-клиент для «Сетевого города»',
     long_description=long_description,
     long_description_content_type='text/markdown',
